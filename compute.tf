46251ab232e80aad012b5bcabc459e01bc3f660d
Caleb Arika <cnarika@yahoo.com>
2022-09-22 17:32:55 -0500

    update edits

--- compute.tf
+++ compute.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" {
   ami             = "${data.aws_ami.amazon-linux-2.id}"
-  instance_type   = "t2.small"
+  instance_type   = "t2.micro"
   key_name        = "${var.keyname}"
   vpc_id          = "${aws_vpc.development-vpc.id}"
   #vpc_security_group_ids = ["${aws_security_group.sg_allow_ssh_jenkins.id}"]
   vpc_security_group_ids = ["${aws_security_group.sg_allow_ssh_ec2.id}"]
   subnet_id          = "${aws_subnet.public-subnet-1.id}"
-  #name            = "${var.name}"
+  name            = "${var.name}"
   #user_data = "${file("Data_install_jenkins.sh")}"
 
   associate_public_ip_address = true
