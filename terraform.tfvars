7c87c16ea5d268e51bc3f790a6081c7626f1117b
Caleb Arika <cnarika@yahoo.com>
2022-09-22 16:53:34 -0500

    commit edits

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,1 +1,13 @@
 # This module is necessary only if working ON-PREMISE
+region = "us-east-1"
+environment = "development"
+vpc_cidr = "10.0.0.0/16"
+public_subnet_1-cidr = "10.0.0.0/24"
+public_subnet_2-cidr = "10.0.0.0/24"
+public_subnet_3-cidr = "10.0.0.0/24"
+private_subnet_1-cidr = "10.0.0.0/24"
+private_subnet_2-cidr = "10.0.0.0/24"
+private_subnet_3-cidr = "10.0.0.0/24"
+instance_type = "t2.medium"
+#instance_ami = ""
+keyname = "cassa1-key"
--- variable.tf
+++ variable.tf
@@ -1,6 +1,34 @@
 variable "region" {
-  description = "My region"
-  default = "us-east-1"  #is optional
+  description = "My region" #is optional
+  default = "us-east-1"
 }
+   variable "environment" {
+    default = "development"
+   }
+   variable "vpc_cidr" {
+    description = "VPC Cidr block"
+   }
+   variable "public_subnet_1_cidr" {
+  description = "Public Subnet 1 cidr block"
+}
+variable "public_subnet_2_cidr" {
+  description = "Public Subnet 2 cidr block"
+}
+variable "public_subnet_3_cidr" {
+  description = "Public Subnet 3 cidr block"
+}
+variable "private_subnet_1_cidr" {
+  description = "Private Subnet 1 cidr block"
+}
+variable "private_subnet_2_cidr" {
+  description = "Private Subnet 2 cidr block"
+}
+variable "private_subnet_3_cidr" {
+  description = "Private Subnet 3 cidr block"
+}
+variable "instance_type" {
+instance = "${var.instance}"
+}
+
 # another way of doing 1-3; enter variable value in .auto.tfvariable "region" { }
   
