31ba656b18eba9b78e04dbca26aa3a53c3a97b28
Caleb Arika <cnarika@yahoo.com>
2022-09-23 07:20:04 -0500

    commit edits

--- .auto.tfvars
+++ .auto.tfvars
@@ -1,6 +1,6 @@
 region = "us-east-1"
 environment = "development"
-vpc_cidr = "10.0.0.0/16"
+vpc-cidr = "10.0.0.0/16"
 public_subnet_1-cidr = "10.0.0.0/24"
 public_subnet_2-cidr = "10.0.0.0/24"
 public_subnet_3-cidr = "10.0.0.0/24"
--- variable.tf
+++ variable.tf
@@ -28,7 +28,7 @@ variable "private_subnet_3_cidr" {
 }
 
 #vpc_cidr = "10.0.0.0/16"
-vpc_cidr = "${var.vpc-cidr}"
+vpc-cidr = "${var.vpc-cidr}"
 public_subnet_1-cidr = "10.0.0.0/24"
 public_subnet_2-cidr = "10.0.0.0/24"
 public_subnet_3-cidr = "10.0.0.0/24"
